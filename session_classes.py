ae64d0d49f50211270f564fa022d0304b7ec969a
Matvey <mdraevich@solidex.by>
2022-05-22 23:18:08 +0300

    some getters implemented; string representation implemented

--- session_classes.py
+++ session_classes.py
@@ -1,4 +1,5 @@
 import time
+import json
 from payload_window import PayloadWindow
 
 
@@ -13,7 +14,7 @@ class TCPSession():
         dst_ip: y.y.y.y
         dst_port: YYYYY
         
-        tcp_payload: PayloadWindow()
+        payload: PayloadWindow()
         custom: {
             **custom key-value pairs**
         }
@@ -21,27 +22,54 @@ class TCPSession():
 
     """
 
-    TTL = 300  # seconds
-
-    def __init__(self, src_ip, src_port, dst_ip, dst_port, tcp_payload=""):
+    def __init__(self, src_ip, src_port, dst_ip, dst_port, payload=""):
         self.src_ip = src_ip
         self.src_port = src_port
         #
         self.dst_ip = dst_ip
         self.dst_port = dst_port
         #
-        self.tcp_payload = PayloadWindow()
-        self.update_with_payload(tcp_payload=tcp_payload)
+        self.payload = PayloadWindow()
+        self.update_with_payload(payload=payload)
         #
-        self.custom = {}
+        self.attributes = {}
 
 
-    def update_with_payload(self, tcp_payload):
-        self.tcp_payload.put(content=tcp_payload)
+    def update_with_payload(self, payload):
+        self.payload.put(content=payload)
         return True
 
 
     def update_with_attribute(self, key, value=None):
-        self.custom[key] = value
+        self.attributes[key] = value
         return True
 
+
+    def get_source(self):
+        return (self.src_ip, self.src_port)
+
+
+    def get_destination(self):
+        return (self.dst_ip, self.dst_port)
+
+
+    def get_payload(self):
+        return str(self.payload)
+
+
+    def get_attributes(self):
+        return self.attributes
+
+
+    def get_session_info(self):
+        session_info = {
+            "Src IP/Port": "{}:{}".format(*self.get_source()),
+            "Dst IP/Port": "{}:{}".format(*self.get_destination()),
+            "TCP Payload": self.get_payload(),
+            "Attributes": self.get_attributes(),
+        }
+        return session_info
+
+
+    def __str__(self):        
+        return json.dumps(self.get_session_info(), sort_keys=True, indent=4)
